a61d7123fee0e41f4d7d5b25337529835feff487
chris levy <christopher@dashhudson.com>
2024-09-11 18:20:41 -0300

    W I P S

--- posts/modal_fun/copali.py
+++ posts/modal_fun/copali.py
@@ -12,7 +12,7 @@ operating_sys = "ubuntu22.04"
 tag = f"{cuda_version}-{flavor}-{operating_sys}"
 image = (
     modal.Image.from_registry(f"nvidia/cuda:{tag}", add_python="3.11")
-    .apt_install("git")
+    .apt_install("git", "poppler-utils")
     .pip_install(
         "ninja",  # required to build flash-attn
         "packaging",  # required to build flash-attn
@@ -23,6 +23,7 @@ image = (
         "pip install git+https://github.com/huggingface/transformers",
         "pip install accelerate",
         "pip install colpali-engine",
+        "pip install requests pdf2image PyPDF2",
         "pip install python-dotenv",
         f'huggingface-cli login --token {os.environ["HUGGING_FACE_ACCESS_TOKEN"]}',
     )
@@ -32,77 +33,58 @@ image = (
 
 @app.cls(image=image, secrets=[modal.Secret.from_dotenv()], gpu="a10g", cpu=4, timeout=600, container_idle_timeout=300)
 class Model:
-
-    def pdf_to_images(self, pdf_url):
-        pass
-    
     @build()
     @enter()
     def setup(self):
-        from colpali_engine.models.paligemma_colbert_architecture import ColPali
+        from typing import cast
+
         import torch
-        from transformers import AutoProcessor
 
-        # Load model
-        model_name = "vidore/colpali-v1.2"
-        model = ColPali.from_pretrained("vidore/colpaligemma-3b-pt-448-base", torch_dtype=torch.bfloat16, device_map="cuda").eval()
-        model.load_adapter(model_name)
-        self.model = model.eval()
-        self.processor = AutoProcessor.from_pretrained(model_name)
+        from colpali_engine.models import ColPali, ColPaliProcessor
 
+        self.model = cast(
+            ColPali,
+            ColPali.from_pretrained(
+                "vidore/colpali-v1.2",
+                torch_dtype=torch.bfloat16,
+                device_map="cuda:0",  # or "mps" if on Apple Silicon
+            ),
+        )
+
+        self.processor = cast(ColPaliProcessor, ColPaliProcessor.from_pretrained("google/paligemma-3b-mix-448"))
 
     @modal.method()
     def f(self):
-        os.system('pip install datasets')
-        import torch
-        from torch.utils.data import DataLoader
-        from tqdm import tqdm
-
         from PIL import Image
+        import torch
 
-        from colpali_engine.utils.colpali_processing_utils import process_images, process_queries
-        from colpali_engine.utils.image_from_page_utils import load_from_dataset
+        os.system("pip install datasets")
+        # Your inputs
+        images = [
+            Image.new("RGB", (32, 32), color="white"),
+            Image.new("RGB", (16, 16), color="black"),
+        ]
+        queries = [
+            "Is attention really all you need?",
+            "Are Benjamin, Antoine, Merve, and Jo best friends?",
+        ]
 
-        """Example script to run inference with ColPali"""
-        # select images -> load_from_pdf(<pdf_path>),  load_from_image_urls(["<url_1>"]), load_from_dataset(<path>)
-        images = load_from_dataset("vidore/docvqa_test_subsampled")
-        queries = ["From which university does James V. Fiorca come ?", "Who is the japanese prime minister?"]
+        # Process the inputs
+        batch_images = self.processor.process_images(images).to(self.model.device)
+        batch_queries = self.processor.process_queries(queries).to(self.model.device)
 
-        # run inference - docs
-        dataloader = DataLoader(
-            images,
-            batch_size=4,
-            shuffle=False,
-            collate_fn=lambda x: process_images(self.processor, x),
-        )
-        ds = []
-        for batch_doc in tqdm(dataloader):
-            with torch.no_grad():
-                batch_doc = {k: v.to(self.model.device) for k, v in batch_doc.items()}
-                embeddings_doc = self.model(**batch_doc)
-            ds.extend(list(torch.unbind(embeddings_doc.to("cpu"))))
+        # Forward pass
+        with torch.no_grad():
+            image_embeddings = self.model(**batch_images)
+            query_embeddings = self.model(**batch_queries)
 
-        modal.interact()
-        import IPython
-        IPython.embed()
-
-        # run inference - queries
-        dataloader = DataLoader(
-            queries,
-            batch_size=4,
-            shuffle=False,
-            collate_fn=lambda x: process_queries(self.processor, x, Image.new("RGB", (448, 448), (255, 255, 255))),
-        )
-
-        qs = []
-        for batch_query in dataloader:
-            with torch.no_grad():
-                batch_query = {k: v.to(self.model.device) for k, v in batch_query.items()}
-                embeddings_query = self.model(**batch_query)
-            qs.extend(list(torch.unbind(embeddings_query.to("cpu"))))
-
-        print(qs)
-        print(ds)
+        scores = self.processor.score_multi_vector(query_embeddings, image_embeddings)
+        print(batch_images)
+        print(batch_queries)
+        print(image_embeddings)
+        print(query_embeddings)
+        print(scores)
+        return scores
 
 
 @app.local_entrypoint()
