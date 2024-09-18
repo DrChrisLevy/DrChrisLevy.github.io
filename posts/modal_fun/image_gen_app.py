32cf735e48b4520ea3a067cd4e73943b76b71b48
chris levy <christopher@dashhudson.com>
2024-09-17 23:07:43 -0300

    wips

--- posts/modal_fun/image_gen_app.py
+++ posts/modal_fun/image_gen_app.py
@@ -1,15 +1,15 @@
 from fastcore.parallel import threaded
 from fasthtml.common import *
-import uuid, os, uvicorn, requests
-
-
+import uuid
+import os
+import requests
 
 
 # gens database for storing generated image details
-tables = database('data/gens.db').t
+tables = database("data/gens.db").t
 gens = tables.gens
-if not gens in tables:
-    gens.create(prompt=str, id=int, folder=str, pk='id')
+if gens not in tables:
+    gens.create(prompt=str, id=int, folder=str, pk="id")
 Generation = gens.dataclass()
 
 
@@ -19,51 +19,66 @@ gridlink = Link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/f
 # Our FastHTML app
 app = FastHTML(hdrs=(picolink, gridlink))
 
+
 # Main page
 @app.get("/")
 def home():
     inp = Input(id="new-prompt", name="prompt", placeholder="Enter a prompt")
-    add = Form(Group(inp, Button("Generate")), hx_post="/", target_id='gen-list', hx_swap="afterbegin")
-    gen_containers = [generation_preview(g) for g in gens(limit=10)] # Start with last 10
-    gen_list = Div(*reversed(gen_containers), id='gen-list', cls="row") # flexbox container: class = row
-    return Title('Image Generation Demo'), Main(H1('Magic Image Generation'), add, gen_list, cls='container')
+    add = Form(Group(inp, Button("Generate")), hx_post="/", target_id="gen-list", hx_swap="afterbegin")
+    gen_containers = [generation_preview(g) for g in gens(limit=10)]  # Start with last 10
+    gen_list = Div(*reversed(gen_containers), id="gen-list", cls="row")  # flexbox container: class = row
+    return Title("Image Generation Demo"), Main(H1("Magic Image Generation"), add, gen_list, cls="container")
+
 
 # Show the image (if available) and prompt for a generation
 def generation_preview(g):
     grid_cls = "box col-xs-12 col-sm-6 col-md-4 col-lg-3"
     image_path = f"{g.folder}/{g.id}.png"
     if os.path.exists(image_path):
-        return Div(Card(
-                       Img(src=image_path, alt="Card image", cls="card-img-top"),
-                       Div(P(B("Prompt: "), g.prompt, cls="card-text"),cls="card-body"),
-                   ), id=f'gen-{g.id}', cls=grid_cls)
-    return Div(f"Generating gen {g.id} with prompt {g.prompt}", 
-            id=f'gen-{g.id}', hx_get=f"/gens/{g.id}", 
-            hx_trigger="every 2s", hx_swap="outerHTML", cls=grid_cls)
+        return Div(
+            Card(
+                Img(src=image_path, alt="Card image", cls="card-img-top"),
+                Div(P(B("Prompt: "), g.prompt, cls="card-text"), cls="card-body"),
+            ),
+            id=f"gen-{g.id}",
+            cls=grid_cls,
+        )
+    return Div(
+        f"Generating gen {g.id} with prompt {g.prompt}",
+        id=f"gen-{g.id}",
+        hx_get=f"/gens/{g.id}",
+        hx_trigger="every 2s",
+        hx_swap="outerHTML",
+        cls=grid_cls,
+    )
+
 
 # A pending preview keeps polling this route until we return the image preview
 @app.get("/gens/{id}")
-def preview(id:int):
+def preview(id: int):
     return generation_preview(gens.get(id))
+
 
 # For images, CSS, etc.
 @app.get("/{fname:path}.{ext:static}")
-def static(fname:str, ext:str): return FileResponse(f'{fname}.{ext}')
+def static(fname: str, ext: str):
+    return FileResponse(f"{fname}.{ext}")
+
 
 # Generation route
 @app.post("/")
-def post(prompt:str):
+def post(prompt: str):
     folder = f"data/gens/{str(uuid.uuid4())}"
     os.makedirs(folder, exist_ok=True)
     g = gens.insert(Generation(prompt=prompt, folder=folder))
     generate_and_save(g.prompt, g.id, g.folder)
-    clear_input =  Input(id="new-prompt", name="prompt", placeholder="Enter a prompt", hx_swap_oob='true')
+    clear_input = Input(id="new-prompt", name="prompt", placeholder="Enter a prompt", hx_swap_oob="true")
     return generation_preview(g), clear_input
+
 
 # Generate an image and save it to the folder (in a separate thread)
 @threaded
 def generate_and_save(prompt, id, folder):
-    import requests
     import base64
     import os
 
@@ -90,7 +105,7 @@ def generate_and_save(prompt, id, folder):
         for result in results:
             img_data = base64.b64decode(result["image"])
 
-            with open(os.path.join("", f'{fname}.png'), "wb") as f:
+            with open(os.path.join("", f"{fname}.png"), "wb") as f:
                 f.write(img_data)
             print(f"Saved: {fname}.png")
     else:
@@ -100,5 +115,5 @@ def generate_and_save(prompt, id, folder):
     print("All images have been downloaded to the 'gens/' folder.")
     return True
 
-serve()
 
+serve()
