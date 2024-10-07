7187c5d619465455cb5ed50a8699ada16476dc94
chris levy <christopher@dashhudson.com>
2024-10-07 08:54:47 -0300

    wips

---
+++ posts/colpali/main.py
@@ -0,0 +1,71 @@
+from fasthtml.common import *
+
+picocss = "https://cdn.jsdelivr.net/npm/@picocss/pico@latest/css/pico.indigo.min.css"
+picolink = (Link(rel="stylesheet", href=picocss), Style(":root { --pico-font-size: 100%; }"))
+app, rt = fast_app(hdrs=(MarkdownJS(), *picolink))
+
+
+@rt("/")
+def get():
+    return Titled(
+        "Ask Questions about any PDF Document",
+        Form(hx_post="/process", hx_target="#results", hx_swap="innerHTML")(
+            Group(
+                Label("PDF URL:", Input(name="pdf_url", type="url", required=True, value="https://arxiv.org/pdf/2410.02525")),
+                Label(
+                    "Number of Pages for Context",
+                    Select(name="top_k", required=True)(
+                        Option("1", value="1", selected=True),
+                        Option("2", value="2"),
+                        Option("3", value="3"),
+                        Option("4", value="4"),
+                        Option("5", value="5"),
+                    ),
+                ),
+            ),
+            Label(
+                "Additional Instructions (Optional)",
+                Textarea(name="additional_instructions", required=False, type="text")("Use markdown headers and bullet points in your responses."),
+            ),
+            Label(
+                "Question",
+                Input(name="question", required=True, type="text", value="What is this paper about? Give me a detailed summary"),
+            ),
+            Button("Submit", type="submit"),
+        ),
+        Div(id="results"),
+    )
+
+
+@rt("/process")
+def post(pdf_url: str, question: str, top_k: int, additional_instructions: str):
+    import modal
+
+    print(f"{pdf_url=}")
+    print(f"{question=}")
+    print(f"{top_k=}")
+    answer_questions_with_image_context = modal.Function.lookup("multi-modal-rag", "answer_questions_with_image_context")
+    res, all_images_data = answer_questions_with_image_context.remote(
+        pdf_url=pdf_url,
+        queries=[question],
+        top_k=top_k,
+        use_cache=True,
+        max_new_tokens=8000,
+        additional_instructions=additional_instructions,
+        model="gpt-4o-mini",  # "gpt-4o-mini" or ="Qwen/Qwen2-VL-7B-Instruct"
+    )
+    image_elements = Grid(
+        *(
+            Div(
+                Img(
+                    src=im,
+                    style="width: 100%; height: auto;",
+                )
+            )
+            for im in all_images_data[0]
+        )
+    )
+    return Div(H2("Answer"), *(Div(r, cls="marked") for r in res), H2("Pages Used for Context"), image_elements)
+
+
+serve()
