279f29309c7215b59c867296b4c03c4ad8354a4d
chris levy <christopher@dashhudson.com>
2024-10-21 16:26:53 -0300

    W I P S

--- posts/colpali/tests/test_multi_modal_rag.py
+++ posts/colpali/tests/test_multi_modal_rag.py
@@ -2,31 +2,40 @@ import modal
 import pytest
 
 
-@pytest.mark.parametrize("model", ["Qwen/Qwen2-VL-7B-Instruct", "gpt-4o-mini"])
-def test_answer_questions_with_image_context(model):
-    answer_questions_with_image_context = modal.Function.lookup("multi-modal-rag", "answer_questions_with_image_context")
-    res, all_images_data = answer_questions_with_image_context.remote(
-        pdf_url="https://arxiv.org/pdf/2405.04434",
-        queries=[
+@pytest.mark.parametrize(
+    "model,query,expected_strs",
+    [
+        (
+            "gpt-4o-mini",
             "What were the results of the Needle In A Haystack evaluation? And what type of cluster and GPU was used for experiments?",
+            ["NVLink", "GPU", "NVIDIA", "H800"],
+        ),
+        (
+            "gpt-4o-mini",
             "What is the conclusion of this paper?",
-        ],
+            [
+                "DeepSeek-V2",
+                "MoE",
+            ],
+        ),
+    ],
+)
+def test_answer_questions_with_image_context(model, query, expected_strs):
+    answer_questions_with_image_context = modal.Function.lookup("multi-modal-rag", "answer_questions_with_image_context")
+    res = answer_questions_with_image_context.remote_gen(
+        pdf_url="https://arxiv.org/pdf/2405.04434",
+        queries=[query],
         top_k=5,
         use_cache=True,
         max_new_tokens=1024,
         model=model,
     )
+    chunks = [r for r in res]
+    answer_str = "".join([c for c in chunks if isinstance(c, str)])
 
-    assert len(res) == 2
-    assert len(all_images_data) == 2
-    assert len(all_images_data[0]) == 5 == len(all_images_data[1])
-    assert all(["data:image" in img for img in all_images_data[0]])
-    assert all(["data:image" in img for img in all_images_data[1]])
-    assert all(t in res[0] for t in ["NVLink", "GPU", "NVIDIA", "H800"])
-    assert all(
-        t in res[1]
-        for t in [
-            "DeepSeek-V2",
-            "MoE",
-        ]
-    )
+    all_images_data = [c for c in chunks if not isinstance(c, str)]
+
+    assert len(all_images_data) == 1
+    assert len(all_images_data[0][0]) == 5
+    assert all(["data:image" in img for img in all_images_data[0][0]])
+    assert all(t in answer_str for t in expected_strs)
