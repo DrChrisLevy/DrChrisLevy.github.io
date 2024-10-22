a120d3b1600931ad9597465cb76175ae51fc8375
chris levy <christopher@dashhudson.com>
2024-10-22 14:34:52 -0300

    W I P S

--- posts/colpali/conftest.py
+++ posts/colpali/conftest.py
@@ -1,5 +1,16 @@
 import os
 import sys
 
+from utils import read_from_queue
+
 # Add the root project directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+def pytest_sessionfinish(session, exitstatus):
+    while True:
+        message = read_from_queue()
+        if not message:
+            break
+    print("\nTest Suite Finished.")
+    print(f"Modal Queue Empty. Exit status: {exitstatus}")
