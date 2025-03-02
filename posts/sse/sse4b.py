ce6bb4a9884551bc17cbfbb1cf61f3a59b0e5c85
chris levy <christopher@dashsocial.comm>
2025-03-02 16:28:48 -0400

    W I P S

---
+++ posts/sse/sse4b.py
@@ -0,0 +1,50 @@
+# ruff: noqa: F403, F405
+from asyncio import sleep
+
+from fasthtml.common import *
+from monsterui.all import *
+
+app, rt = fast_app(
+    hdrs=(
+        Theme.blue.headers(highlightjs=True),
+        Script(src="https://unpkg.com/htmx-ext-sse@2.2.1/sse.js"),
+    ),
+    live=True,
+)
+
+
+@rt("/")
+def index():
+    return Container(
+        H3("Multiple events in different elements (from the same source)."),
+        Div(
+            DivFullySpaced(
+                # child elements of the element containing the hx_ext and sse-connect attributes.
+                Div(P("event1"), sse_swap="event1"),
+                Div(P("event2"), sse_swap="event2"),
+            ),
+            hx_ext="sse",
+            sse_connect="/sse-stream",
+            hx_swap="beforeend show:bottom",
+            sse_close="close",
+        ),
+    )
+
+
+async def message_generator():
+    for i in range(10):
+        event_name = "event1" if i % 2 == 0 else "event2"
+        yield sse_message(Div(P(f"message number {i} from {event_name}")), event=event_name)
+        await sleep(0.5)
+
+    yield sse_message(Div(P("DONE event1")), event="event1")
+    yield sse_message(Div(P("DONE event2")), event="event2")
+    yield sse_message(Div(), event="close")
+
+
+@rt("/sse-stream")
+async def sse_stream():
+    return EventStream(message_generator())
+
+
+serve(port=5010)
