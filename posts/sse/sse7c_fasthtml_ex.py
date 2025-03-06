ad78e6c65949f560b2cd6fbba2bce83b7ccb87a6
chris levy <christopher@dashsocial.comm>
2025-03-06 07:54:02 -0400

    W I P S

--- posts/sse/sse7c_fasthtml_ex.py
+++ posts/sse/sse7c_fasthtml_ex.py
@@ -12,23 +12,21 @@ app, rt = fast_app(
     live=True,
 )
 
-
-def ChatInput():
-    return Input(
-        type="text",
-        name="msg",
-        id="msg-input",
-        placeholder="Type a message",
-        hx_swap_oob="true",  # TODO: WHAT DOES IT DO?
-    )
+final_message = ""
 
 
 @app.route("/")
 def get():
-    page = Body(
+    return Container(
         H1("Streamed Chat"),
         Form(
-            (ChatInput(), Button("Send")),
+            Input(
+                type="text",
+                name="msg",
+                id="msg-input",
+                placeholder="Type a message",
+            ),
+            Button("Send"),
             hx_post="/send-message",
             hx_target="#chat-response",
             hx_swap="innerHTML",
@@ -37,26 +35,23 @@ def get():
             id="chat-response",
         ),
     )
-    return Title("Chatbot Demo"), page
 
 
 @app.post("/send-message")
 def send_message(msg: str):
     print(f"SEND MESSAGE: {msg}")
-    assistant_msg = Div(
-        Div(
-            Div(id="chat-content", hx_ext="sse", sse_connect="/get-message", sse_swap="EventName", sse_close="close", hx_swap="beforeend"),
-            id="chat-message",
-        )
-    )
-    return ChatInput(), assistant_msg
+    assistant_msg = Div(id="chat-content", hx_ext="sse", sse_connect="/get-message", sse_swap="EventName", sse_close="close", hx_swap="beforeend")
+
+    return assistant_msg
 
 
 async def message_generator():
-    r = ["This ", "is ", "a ", "test"]
+    global final_message
+    r = ["# This ", "is ", "a ", "- test\n- test2"]
     for chunk in r:
+        final_message += chunk
         yield sse_message(chunk, event="EventName")
-        await sleep(0.1)
+        await sleep(0.5)
     yield sse_message(Div(), event="close")
 
 
