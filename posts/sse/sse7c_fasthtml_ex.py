1aa160042fd74886626c3bbe1f31d72efabf0e9f
chris levy <christopher@dashsocial.comm>
2025-03-05 21:51:42 -0400

    W I P S

--- posts/sse/sse7c_fasthtml_ex.py
+++ posts/sse/sse7c_fasthtml_ex.py
@@ -1,102 +1,68 @@
 # ruff: noqa: F403, F405
-import asyncio
+from asyncio import sleep
 
 from fasthtml.common import *
 from monsterui.all import *
 
 app, rt = fast_app(
     hdrs=(
-        Theme.blue.headers(highlightjs=True),
+        Theme.blue.headers(),
         Script(src="https://unpkg.com/htmx-ext-sse@2.2.1/sse.js"),
     ),
     live=True,
 )
 
-# Set up a chat model client and list of messages (https://claudette.answer.ai/)
 
-messages = []
-
-
-# Send messages to the chat model and yield the responses
-async def message_generator():
-    print("message_generator", messages)
-    r = ["This ", "is ", "a ", "test"]
-    for chunk in r:
-        messages[-1]["content"] += chunk
-        yield sse_message(chunk)
-        await asyncio.sleep(0.5)
-    yield sse_message(Div(), event="close")
-
-
-# Chat message component (renders a chat bubble)
-# Now with a unique ID for the content and the message
-def ChatMessage(msg_idx, **kwargs):
-    msg = messages[msg_idx]
-    bubble_class = "chat-bubble-primary" if msg["role"] == "user" else "chat-bubble-secondary"
-    chat_class = "chat-end" if msg["role"] == "user" else "chat-start"
-    return Div(
-        Div(msg["role"], cls="chat-header"),
-        Div(
-            msg["content"],
-            id=f"chat-content-{msg_idx}",  # Target if updating the content
-            cls=f"chat-bubble {bubble_class}",
-            **kwargs,
-        ),
-        id=f"chat-message-{msg_idx}",  # Target if replacing the whole message
-        cls=f"chat {chat_class}",
-    )
-
-
-# The input field for the user message. Also used to clear the
-# input field after sending a message via an OOB swap
 def ChatInput():
     return Input(
         type="text",
         name="msg",
         id="msg-input",
         placeholder="Type a message",
-        cls="input input-bordered w-full",
-        hx_swap_oob="true",
+        hx_swap_oob="true",  # TODO: WHAT DOES IT DO?
     )
 
 
-# The main screen
 @app.route("/")
 def get():
     page = Body(
-        H1("Chatbot SSE (server-sent events) Demo"),
-        Div(
-            *[ChatMessage(msg) for msg in messages],
-            id="chatlist",
-            cls="chat-box h-[73vh] overflow-y-auto",
-        ),
+        H1("Streamed Chat"),
         Form(
-            Group(ChatInput(), Button("Send", cls="btn btn-primary")),
+            (ChatInput(), Button("Send")),
             hx_post="/send-message",
-            hx_target="#chatlist",
-            hx_swap="beforeend",
-            cls="flex space-x-2 mt-2",
+            hx_target="#chat-response",
+            hx_swap="innerHTML",
         ),
-        cls="p-4 max-w-lg mx-auto",
+        Div(
+            id="chat-response",
+        ),
     )
     return Title("Chatbot Demo"), page
+
+
+@app.post("/send-message")
+def send_message(msg: str):
+    print(f"SEND MESSAGE: {msg}")
+    assistant_msg = Div(
+        Div(
+            Div(id="chat-content", hx_ext="sse", sse_connect="/get-message", sse_swap="EventName", sse_close="close", hx_swap="beforeend"),
+            id="chat-message",
+        )
+    )
+    return ChatInput(), assistant_msg
+
+
+async def message_generator():
+    r = ["This ", "is ", "a ", "test"]
+    for chunk in r:
+        yield sse_message(chunk, event="EventName")
+        await sleep(0.1)
+    yield sse_message(Div(), event="close")
 
 
 @app.get("/get-message")
 async def get_message():
     return EventStream(message_generator())
-
-
-@app.post("/send-message")
-def send_message(msg: str):
-    messages.append({"role": "user", "content": msg})
-    user_msg = Div(ChatMessage(len(messages) - 1))
-    messages.append({"role": "assistant", "content": ""})
-    # The returned assistant message uses the SSE extension, connect to the /get-message endpoint and get all messages until the close event
-    assistant_msg = Div(
-        ChatMessage(len(messages) - 1, hx_ext="sse", sse_connect="/get-message", sse_swap="message", sse_close="close", hx_swap="beforeend")
-    )
-    return user_msg, assistant_msg, ChatInput()
 
 
 serve(port=5010)
